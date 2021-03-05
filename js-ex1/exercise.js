e5c178fe63db4bf42a7e5485224ac606af393e5d
ahlam alshaba <ama1977ln@outlook.com>
2021-03-05 13:25:28 +0100

    done

---
+++ js-ex1/exercise.js
@@ -0,0 +1,22 @@
+var array1 = ["m", "p", 9, 6, 0 , "a"];
+var array2 = ["c", "n", 9, 8, 1 , "c", 9];
+var num = 0;
+var leter= 0;
+function getArray(array) {
+    
+    array.forEach( function(item) {
+        if (typeof item === "number"){
+        num = num +1 ;
+        } else {
+            leter = leter + 1;
+        }
+        });  
+console.log("There are " + array.length +" elements in your array.");
+console.log("There are " + num +" numbers");
+console.log("There are " + leter +" letters");
+}
+
+
+
+
+
