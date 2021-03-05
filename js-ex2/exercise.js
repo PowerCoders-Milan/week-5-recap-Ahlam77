224f3be27d2b310169bca856081bbea07aa7bae1
ahlam alshaba <ama1977ln@outlook.com>
2021-03-05 15:42:52 +0100

    done

---
+++ js-ex2/exercise.js
@@ -0,0 +1,15 @@
+var Array1 = ['marco', 'antonio', 'luca', 13, 18, 'leo', 'mary'];
+var Array2 = [21, 19, 12, 'sebastian', 'will', 23, 20];
+
+function myArray (){
+    console.log (Array1.sort()+ Array2.sort());}
+    var result = {
+    'marco': 21,
+    'antonio':19,
+    'luca':12,
+    'leo':13,
+    'mary':23,
+    'will':20,}
+console.log(result);
+
+myArray()
