845f515ecbb2e635d2c70bff50246245f80c8c14
ahlam alshaba <ama1977ln@outlook.com>
2021-03-07 18:14:38 +0100

    not

--- dom-extra/exercise.js
+++ dom-extra/exercise.js
@@ -4,7 +4,7 @@ Movies = [
     { title: "movie3", duration: 160, actor: "Jack Nicholson"},
     { title: "movie4", duration: 100, actor: "Denzel Washington"},
    ]
-
+/*
    var counter = 0;
    var a = document.createElement("ul");
  function myFunction(){
@@ -19,41 +19,4 @@ Movies = [
     a.appendChild(document.createElement("li").appendChild(two));
     a.appendChild(document.createElement("li").appendChild(three));
     document.getElementsByTagName("body")[0].appendChild(a);
-    if(counter == 0){
-        a.style.backgroundColor= "blue";
-    }
-    if(counter == 1){
-        a.style.backgroundColor= "green";
-    }
-    if(counter == 2){
-        a.style.backgroundColor= "yellow";
-    }
-    if(counter == 3){
-        a.style.backgroundColor= "gray";
-    }
-    counter = counter + 1;
-    if(counter == 4) {
-        a.removeEventListener("click", myFunction);
-    }
- }
-
- 
- function myFunction1(){
-     a.style.backgroundColor = "black";
-     a.style.color = "white";
     
- }
-
- 
- function myFunction2(){
-    a.style.backgroundColor = "white";
-    a.style.color = "black";
-   
-}
-
-
-
-
-document.getElementsByTagName("button")[0].addEventListener("click", myFunction);
-a.addEventListener("mouseover", myFunction1);
-a.addEventListener("mouseout", myFunction2);
